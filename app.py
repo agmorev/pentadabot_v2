c0553acc70dcf77303a018d956545f6a9444b351
agmorev <agmorev@gmail.com>
2020-11-07 23:58:12 +0200

    Create webhook

--- app.py
+++ app.py
@@ -16,4 +16,11 @@ if __name__ == '__main__':
     from aiogram import executor
     from handlers import dp
 
-    executor.start_polling(dp, on_startup=on_startup)
+    # executor.start_polling(dp, on_startup=on_startup)
+    executor.start_webhook(
+        dispatcher=dp,
+        webhook_path='',
+        skip_updates=True,
+        host='https://pentadabot.herokuapp.com/',
+        port='',
+    )
